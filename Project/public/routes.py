5c8f1978a4b1a1c20465e0f4da2801fb7fba6a4a
BHS13048 <30673147+BHS13048@users.noreply.github.com>
2017-08-03 11:46:04 +1200

    version 1

---
+++ Project/public/routes.py
@@ -0,0 +1,55 @@
+from flask import request
+from flask import g
+from flask import render_template
+from public import website
+from public import datamanager
+
+from flask import redirect
+from datetime import datetime
+
+#home for driving application
+
+@website.route('/')
+def home_page():
+    query_string = (
+        'SELECT question, answer_one, answer_two, answer_three, answer_four '
+        'FROM questions '
+    )
+    query_results = datamanager.query_db(query_string, [], one=False)
+    return render_template('index.html', questions=query_results)
+
+@website.route('/quiz-page', methods=['GET', 'POST'])#name that the base-template calls for when the link is clicked
+def quiz_page():
+    print(request.method)
+    if request.method == 'GET':
+        query_string = (
+            'SELECT question, answer_one, answer_two, answer_three, answer_four '
+            'FROM questions '
+            'ORDER BY question_no ASC '
+        )
+        query_results = datamanager.query_db(query_string, [], one=False)        
+        return render_template('quiz-page.html', questions=query_results)
+    
+    elif request.method == 'POST':
+        
+        #calls for data entered into the forms by user to be entered into the database
+        user_answer = request.form.get('answer') #calls for answer in form
+        
+        #expected data
+        query_string = ( #places variable contents into the database under the correct tables and columns
+            'SELECT question, answer_one, answer_two, answer_three, answer_four, correct_answer, user_answer '
+            'FROM questions, users '
+            'ORDER BY question_no ASC '
+            'INSERT INTO users( user_answer ) '
+            'VALUES (?)'
+        )
+        query_result = datamanager.query_db(
+            query_string,
+            [user_answer],
+            one=True
+        )
+        #if user_answer == correct_answer:
+        return render_template('quiz-page.html', questions=query_results, active='user_answer')
+        #else:
+            #return render_template('index.html')
+    
