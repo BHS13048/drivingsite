d318ee2fc4816d235696eda4cedfa3daac8eefa8
BHS13048 <30673147+BHS13048@users.noreply.github.com>
2017-10-05 09:42:38 +1300

    Version 6.2
    Just fixing minor errors and spelling.

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ current_question = ""
 app = Flask(__name__)
 
 original_questions = {
-	'What must you do when red lights are flasking at a railway level crossing?':['Stop unti the lights stop flashing','Check both sides and cross if no trains are coming','Go as soon as the train has passed'],
+	'What must you do when red lights are flashing at a railway level crossing?':['Stop unti the lights stop flashing','Check both sides and cross if no trains are coming','Go as soon as the train has passed'],
 	'You can park on a dashed yellow line on the side of the road':['True','False'],
 	'What must you do when turning right at a roundabout?':['Indicate right as you approach, then left before you exit','Indicate right the whole way around the roundabout','Don\'t indiate right as you approach, but indicate right once on the roundabout','Stay left as you enter the roundabout'],
 	'New Zealand\'s road signs are shown in:':['Kilometres per hour','Shown in Miles per hour'],
  	'Who is responsible for making sure passengers below the age of 16 have their seatbelts on?':['Mum','Dad','The driver','Yourself, doesn\'t matter how old you are'],
 	'What must you do at a pedestrian crossing?':['Just wait until there are no pedestrians on your side of the crossing','Make sure pedestrians are completely off the road before crossing','Continue, the pedestrians will wait for you'],
-	'What must you do if you are looking for something and travelling well below the speed limit?':['Move to the side to let traffic flow behind you','Continue at the same speed and block traffic flow'],
+	'What must you do if you are looking for something, there is traffic behind you, and travelling well below the speed limit?':['Move to the side to let traffic flow behind you','Continue at the same speed and block traffic flow'],
 	'At an uncontrolled intersection, when you are travelling straight you must give way to who?':['Traffic on the right','Traffic on the left','All'],
 	'What must you do if you\'re driving and become sleepy?':['Pull over as soon as possible and take a break','Open the window','Speed up to keep up your brain activity'],
 	'Can you pass other traffic over a solid yellow line?':['No, that is very dangerous','Yes, if there is 100m of clear space and there is enough space beside to pass without crossing the yellow line','Yellow Lines are the same as white lines']
@@ -31,14 +31,17 @@ def home_page():
 @app.route('/quiz')
 def quiz():
 	print('gets here')
+	global questions
 	global turns
 	global current_question
-	if turns == 0:
+	if turns == 0 or questions == {}:
 		turns = 10
+		questions = copy.deepcopy(original_questions)
 		return render_template('index.html')
 	else:
 		turns = turns - 1
 		print(turns)
+		print(questions)
 		current_question = random.choice(list(questions)) #calls for definition, substitutes q for questions.
 		#print(current_question)
 		print(current_question)
