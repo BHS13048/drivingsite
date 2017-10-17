f5585ad8c3d10fb1f981c647289fc10846e6b29c
BHS13048 <30673147+BHS13048@users.noreply.github.com>
2017-10-18 08:44:24 +1300

    V7.2

--- app.py
+++ app.py
@@ -6,32 +6,29 @@ current_question = ""
 
 app = Flask(__name__)
 
+#question list
 original_questions = {
 	'What must you do when red lights are flashing at a railway level crossing?':['Stop unti the lights stop flashing','Check both sides and cross if no trains are coming','Go as soon as the train has passed'],
 	'You can park on a dashed yellow line on the side of the road':['False','True'],
 	'What must you do when turning right at a roundabout?':['Indicate right as you approach, then left before you exit','Indicate right the whole way around the roundabout','Don\'t indiate right as you approach, but indicate right once on the roundabout','Stay left as you enter the roundabout'],
 	'New Zealand\'s road signs are shown in:':['Kilometres per hour','Shown in Miles per hour'],
- 	'Who is responsible for making sure passengers below the age of 16 have their seatbelts on?':['Mum','Dad','The driver','Yourself, doesn\'t matter how old you are'],
+ 	'Who is responsible for making sure passengers below the age of 16 have their seatbelts on?':['The driver','Mum','Dad','Yourself, doesn\'t matter how old you are'],
 	'What must you do at a pedestrian crossing?':['Just wait until there are no pedestrians on your side of the crossing','Make sure pedestrians are completely off the road before crossing','Continue, the pedestrians will wait for you'],
 	'What must you do if you are looking for something, there is traffic behind you, and travelling well below the speed limit?':['Move to the side to let traffic flow behind you','Continue at the same speed and block traffic flow'],
 	'At an uncontrolled intersection, when you are travelling straight you must give way to who?':['Traffic on the right','Traffic on the left','All'],
 	'What must you do if you\'re driving and become sleepy?':['Pull over as soon as possible and take a break','Open the window','Speed up to keep up your brain activity'],
-	'Can you pass other traffic over a solid yellow line?':['No, that is very dangerous','Yes, if there is 100m of clear space and there is enough space beside to pass without crossing the yellow line','Yellow Lines are the same as white lines']
+	'Can you pass other traffic over a solid yellow line?':['Yes, if there is 100m of clear space and there is enough space beside to pass without crossing the yellow line','No, that is very dangerous','Yellow Lines are the same as white lines']
 }
 
 questions = copy.deepcopy(original_questions)
-#function shuffles q - questions is later substituted in order to shuffle the questions.
 
-
-
-#home page
+#home page - page that opens at start, has link to quiz page
 @app.route('/')
 def home_page():
 	print('gets to the index page')
 	return render_template('index.html')
 
-
-
+#quiz page - asks user questions, tells the user when and when they don't get the answer correct
 @app.route('/quiz')
 def quiz():
 	print('gets here')
